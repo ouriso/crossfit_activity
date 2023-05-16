89748ab1e78d10cc18c8fa6b70f4f5cf23a35785
Gumerov Artur <2diver7@gmail.com>
2023-05-17 01:16:53 +0300

    Common module with CRUD operations created

---
+++ cf_activity/data/crud_base.py
@@ -0,0 +1,62 @@
+from abc import ABC
+from typing import Annotated, Type, Union
+from uuid import UUID
+
+from fastapi import Depends, HTTPException
+from sqlalchemy import delete, select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeMeta
+
+from data import get_session
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
+
+class BaseManager(ABC):
+    orm_model: Type[DeclarativeMeta] = None
+
+    @classmethod
+    async def get_object_by_id(
+            cls, async_session: SessionDep, item_id: Union[int, UUID]
+    ) -> orm_model:
+        result_stmt = await async_session.execute(
+            select(cls.orm_model).filter_by(id=item_id)
+        )
+        result = result_stmt.scalar()
+        if not result:
+            raise HTTPException(status_code=404, detail="Object not found")
+        return result
+
+    @classmethod
+    async def get_objects_list(
+            cls, async_session: AsyncSession
+    ) -> list[Type[orm_model]]:
+        result = await async_session.execute(
+            select(cls.orm_model)
+        )
+        return list(result.scalars().all())
+
+    @classmethod
+    async def create_object(
+            cls, async_session: AsyncSession, **values
+    ) -> Type[orm_model]:
+        new_instance = cls.orm_model(**values)
+        async_session.add(new_instance)
+        return new_instance
+
+    @classmethod
+    async def update_object(
+            cls, async_session: AsyncSession,
+            item_id: Union[int, UUID], **new_values
+    ) -> Type[orm_model]:
+        await async_session.execute(
+            update(cls.orm_model).filter_by(id=item_id).values(**new_values)
+        )
+
+    @classmethod
+    async def delete_object(
+            cls, async_session: AsyncSession, item_id: Union[int, UUID]
+    ) -> None:
+        await async_session.execute(
+            delete(cls.orm_model).where(id=item_id)
+        )
--- cf_activity/data/datasets/crud.py
+++ cf_activity/data/datasets/crud.py
@@ -1,33 +1,21 @@
-from abc import ABC
 from typing import Annotated, Type
 
-from fastapi import Depends, HTTPException
+from fastapi import Depends
 from slugify import slugify
-from sqlalchemy import delete, select, update
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql.elements import or_
 
 from data import get_session
+from data.crud_base import BaseManager
 from data.datasets.models import BaseDatasetEntity, ExerciseType, \
     ExercisesSetType
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
 
-class BaseManager(ABC):
+class DatasetManager(BaseManager):
     orm_model: Type[BaseDatasetEntity] = None
-
-    @classmethod
-    async def get_object_by_id(
-            cls, async_session: SessionDep, item_id: int
-    ) -> orm_model:
-        result_stmt = await async_session.execute(
-            select(cls.orm_model).filter_by(id=item_id)
-        )
-        result = result_stmt.scalar()
-        if not result:
-            raise HTTPException(status_code=404, detail="Object not found")
-        return result
 
     @classmethod
     async def get_objects_by_name(
@@ -42,44 +30,10 @@ class BaseManager(ABC):
         )
         return list(result.scalars())
 
-    @classmethod
-    async def get_objects_list(
-            cls, async_session: AsyncSession
-    ) -> list[Type[orm_model]]:
-        result = await async_session.execute(
-            select(cls.orm_model)
-        )
-        return list(result.scalars().all())
 
-    @classmethod
-    async def create_object(
-            cls, async_session: AsyncSession, **values
-    ) -> Type[orm_model]:
-        new_instance = cls.orm_model(**values)
-        async_session.add(new_instance)
-        return new_instance
-
-    @classmethod
-    async def update_object(
-            cls, async_session: AsyncSession,
-            item_id: int, **new_values
-    ) -> Type[orm_model]:
-        await async_session.execute(
-            update(cls.orm_model).filter_by(id=item_id).values(**new_values)
-        )
-
-    @classmethod
-    async def delete_object(
-            cls, async_session: AsyncSession, item_id: int
-    ) -> None:
-        await async_session.execute(
-            delete(cls.orm_model).where(id=item_id)
-        )
-
-
-class ExerciseTypeManager(BaseManager):
+class ExerciseTypeManager(DatasetManager):
     orm_model = ExerciseType
 
 
-class ExercisesSetTypeManager(BaseManager):
+class ExercisesSetTypeManager(DatasetManager):
     orm_model = ExercisesSetType
